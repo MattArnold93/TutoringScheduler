00f5572ddfd0dc1c70ecbff862c06846db8f1e5d
hburgess <hburgess@mail.umw.edu>
2015-03-30 17:24:51 +0000

    appointment.py created

---
+++ classes/appointment.py
@@ -0,0 +1,38 @@
+def appointment():
+    tutor = ""
+    student = ""
+    time = ""
+    subject = ""
+    course = ""
+    
+    
+
+def getTutor(appointment):
+    return appointment.tutor
+
+def getStudent(appointment):
+    return appointment.student
+    
+def getTime(appointment):
+    return appointment.time
+    
+def getSubject(appointment):
+    return appointment.subject
+    
+def getCourse(appointment):
+    return appointment.course
+    
+def setTutor(tutor):
+    appointment.tutor = tutor
+
+def setStudent(student):
+    appointment.student = student
+    
+def setTime(time):
+    appointment.time = time
+    
+def setSubject(subject):
+    appointment.subject = subject
+    
+def setCourse(course):
+    appointment.course = course
