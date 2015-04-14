8360169bd6e75b2d72a49856f5c086d5030e4133
hburgess <hburgess@mail.umw.edu>
2015-04-14 01:10:57 +0000

    css edit to look like UMW site

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, datetime
+from flask import Flask, render_template, request, redirect, url_for, session
 import MySQLdb, utils, os, unicodedata
 
 app = Flask(__name__)
